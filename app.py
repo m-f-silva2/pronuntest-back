c3baad8c640982fd618d4907c4d6c80b9fceb0cd
unknown <mfsilva@unimayor.edu.co>
2024-08-08 03:31:33 -0500

    test

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from core import convert_audio_to_spectrograms, PhonemeRecognitionService
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 model = PhonemeRecognitionService()
 
 
