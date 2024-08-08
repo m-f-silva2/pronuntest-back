119f415d283dd939cb904f7d4ba716c794a4fae1
unknown <mfsilva@unimayor.edu.co>
2024-08-08 05:18:48 -0500

    validacion formato de audio

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def validate_phoneme_pattern(pattern: str):
         return jsonify(result)
     except Exception as e:
             print(f"Error while processing the file: {e}")
-            return jsonify({"error": str(e)}), 400
+            return jsonify({"error": str(e+" ||file type:"+recording.content_type)}), 400
 
 @app.route('/')
 def home():
